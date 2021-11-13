5d0a2dce8cb59ed07f8980ef3519e8a1d0345613
Esdras B Silva <bzr4@icloud.com>
2021-11-12 22:20:39 -0300

    - Ajustes na consulta de sugestões Web000HostApp. - Remoção de url para os alunos informarem urls próprias.

--- app/src/main/java/dev/esdras/e104caixasugestoes/SugestaoListActivity.java
+++ app/src/main/java/dev/esdras/e104caixasugestoes/SugestaoListActivity.java
@@ -1,14 +1,13 @@
 package dev.esdras.e104caixasugestoes;
 
-import androidx.annotation.RequiresApi;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -22,11 +21,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import dev.esdras.e104caixasugestoes.adapters.SugestaoAdapter;
 import dev.esdras.e104caixasugestoes.model.Sugestao;
-import dev.esdras.e104caixasugestoes.model.SugestaoDataSource;
 
 public class SugestaoListActivity extends AppCompatActivity {
 
@@ -37,93 +34,50 @@ public class SugestaoListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sugestao_list);
 
-        String url = "https://esdrasilva.000webhostapp.com/caixasugestoes/get_all_suggestions.php1";
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-
+        // Criar mensagem de carregamento, esta mensagem será concluida apenas ao finalizar a requisição ao 000WebHost
         AlertDialog alertDialog = new AlertDialog.Builder(this)
                 .setTitle(R.string.titulo_alerta_carregamento)
                 .setMessage(R.string.mensagem_carregamento)
                 .create();
         alertDialog.show();
 
+        // Criar e configurar a RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewSugestoes);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        //Criação da requisição ao 000WebHost
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        // Neste trecho você irá adicionar a url que realiza a consulta ao banco gerando o JSON.
+        String url = "COLOQUE AQUI SUA URL";
+
+        // Criação da requisição utilizando a API Volley
         StringRequest request = new StringRequest(Request.Method.GET, url, response -> {
-            //Aqui ele te entrega a resposta completa
             Log.d("Response->", response);
             try {
+                // Ao concluir a requisição, iremos converter o JSON recebido em um array de objetos do tipo Sugestao
                 sugestoes = Arrays.asList(new ObjectMapper().readValue(response, Sugestao[].class));
+
+                // Criação do Adapter, passando a lista de objetos do tipo Sugestão como argumento no construtor.
                 SugestaoAdapter sugestaoAdapter = new SugestaoAdapter(sugestoes);
-                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-                RecyclerView recyclerView = findViewById(R.id.recyclerViewSugestoes);
+
+                // Configurando o Adapter de Dados para exibir na RecyclerView.
                 recyclerView.setAdapter(sugestaoAdapter);
-                recyclerView.setLayoutManager(linearLayoutManager);
             } catch (JsonProcessingException e) {
                 e.printStackTrace();
             }
+
+            // Fechando a caixa de dialogo de carregamento
             alertDialog.dismiss();
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e("Erro", error.getLocalizedMessage());
+                // Fechando a caixa de dialogo de carregamento em caso de erros ocorrerem.
                 alertDialog.dismiss();
-                AlertDialog erroDialog;
-                erroDialog = new AlertDialog.Builder(getApplicationContext())
-                        .setTitle("Falha no carregamento")
-                        .setMessage("Oop... Algo deu errado no carregamento!\n".concat(error.getLocalizedMessage()))
-                        .setPositiveButton("OK", null)
-                        .create();
-                alertDialog.show();
+                Toast.makeText(getApplicationContext(), "Oop... Algo deu errado no carregamento!", Toast.LENGTH_SHORT).show();
             }
         });
-
         requestQueue.add(request);
-
-        //TODO: Receber os dados do Web000Host
-//        if (carregarSugestoes()) {
-//            SugestaoAdapter sugestaoAdapter = new SugestaoAdapter(sugestoes);
-//            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-//            RecyclerView recyclerView = findViewById(R.id.recyclerViewSugestoes);
-//            recyclerView.setAdapter(sugestaoAdapter);
-//            recyclerView.setLayoutManager(linearLayoutManager);
-//        } else {
-//            AlertDialog alertDialog1 = new AlertDialog.Builder(this)
-//                    .setTitle(R.string.titulo_alerta_carregamento)
-//                    .setMessage("Não há registros...")
-//                    .create();
-//            alertDialog1.show();
-//        }
-
-
-
-    }
-
-    private boolean carregarSugestoes() {
-        String url = "https://esdrasilva.000webhostapp.com/caixasugestoes/get_all_suggestions.php";
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-
-        AlertDialog alertDialog = new AlertDialog.Builder(this)
-                .setTitle(R.string.titulo_alerta_carregamento)
-                .setMessage(R.string.mensagem_carregamento)
-                .create();
-        alertDialog.show();
-
-        StringRequest request = new StringRequest(Request.Method.GET, url, response -> {
-            //Aqui ele te entrega a resposta completa
-            Log.d("Response->", response);
-            try {
-                sugestoes = Arrays.asList(new ObjectMapper().readValue(response, Sugestao[].class));
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-            alertDialog.dismiss();
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Erro", error.getLocalizedMessage());
-            }
-        });
-
-        requestQueue.add(request);
-
-        return Boolean.FALSE.equals(sugestoes.isEmpty());
     }
 }
